1abe53d8a228ae3c0255d2bfc897e1c1b6c3dcf9
Victor Vilela <victor_vilel@hotmail.com>
2023-01-02 21:24:46 -0300

    deploy to render

---
+++ src/handler.py
@@ -0,0 +1,39 @@
+import pickle
+import pandas as pd
+from flask import Flask, request, Response
+from health_insurance import HealthInsurance
+
+# loading model
+path = '/home/vvilela/repos/health_insurance/models/'
+model = pickle.load(open( path + 'model_linear_regression.pkl', 'rb'))
+
+# initialize API
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def health_insurance_predict():
+    test_json = request.get_json()
+    
+    if test_json: # data
+        if isinstance(test_json, dict): # unique example
+            test_raw = pd.DataFrame(test_json, index=[0])
+            
+        else: # multiple example
+            test_raw = pd.DataFrame(test_json, columns=test_json[0].keys() )
+    
+        # instanciate health insurance class
+        pipeline = HealthInsurance()
+
+        df1 = pipeline.data_engineering(test_raw)
+
+        df2 = pipeline.data_preparation(df1)
+
+        df_response = pipeline.get_prediction(model, test_raw, df2)
+
+        return df_response
+    
+    else:
+        return Response( '{}', status=200, mimetype='application/json' )
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=8000, debug=True)
---
+++ webapp/healthinsurance/HealthInsurance.py
@@ -0,0 +1,58 @@
+import pickle
+import pandas as pd
+import numpy as np
+
+class HealthInsurance(object):
+    
+    def __init__(self):
+        self.home_path = '/home/vvilela/repos/health_insurance/'
+        self.annual_premium_scaler =            pickle.load(open(self.home_path + 'src/features/annual_premium_scaler.pkl', 'rb'))
+        self.age_scaler =                       pickle.load(open(self.home_path + 'src/features/age_scaler.pkl', 'rb'))
+        self.vintage_scaler =                   pickle.load(open(self.home_path + 'src/features/vintage_scaler.pkl', 'rb'))
+        self.target_encode_gender_scaler =      pickle.load(open(self.home_path + 'src/features/target_encode_gender_scaler.pkl', 'rb'))
+        self.target_encode_region_code_scaler = pickle.load(open(self.home_path + 'src/features/target_encode_region_code_scaler.pkl', 'rb'))
+        self.fe_policy_sales_channel_scaler =   pickle.load(open(self.home_path + 'src/features/fe_policy_sales_channel_scaler.pkl', 'rb'))
+        
+    def data_engineering(self, data):
+        # vehicle_damage
+        data['vehicle_damage'] = data['vehicle_damage'].apply( lambda x: 1 if x=='Yes' else 0)
+        
+        # vehicle_age
+        # data['vehicle_age'] = data['vehicle_age'].apply( lambda x: 'over_2_years' if x=='> 2 Years' else
+        #                                                  'between_1_2_year' if x=='1-2 Year' else 'below_1_year')
+        return data
+    
+
+    def data_preparation(self, data):
+
+        # annual_premium
+        data['annual_premium'] = self.annual_premium_scaler.transform(data[['annual_premium']].values)
+        
+        # age
+        data['age'] = self.age_scaler.transform(data[['age']].values)
+
+        # vintage
+        data['vintage'] = self.vintage_scaler.transform(data[['vintage']].values)
+
+        # gender
+        data.loc[:, 'gender'] = data['gender'].map(self.target_encode_gender_scaler)
+
+        # region_code
+        data.loc[:, 'region_code'] = data['region_code'].map(self.target_encode_region_code_scaler)
+
+        # policy_sales_channel
+        data.loc[:, 'policy_sales_channel'] = data['policy_sales_channel'].map(self.fe_policy_sales_channel_scaler)
+
+        cols_selected = ['annual_premium', 'vintage', 'age', 'region_code', 'vehicle_damage', 
+                         'previously_insured', 'policy_sales_channel']
+        
+        return data[cols_selected]
+    
+    def get_prediction(self, model, original_data, test_data):
+        # model prediction
+        pred = model.predict_proba(test_data)
+        
+        # join prediction into original data
+        original_data['prediction'] = pred[:,1].tolist()
+
+        return original_data.to_json(orient='records', date_format='iso')
